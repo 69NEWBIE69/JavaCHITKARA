b58b9b4c8f0e2cd074209c135282f9787673694b
“69NEWBIE69” <deevyamsoin@gmail.com>
2023-04-26 11:16:14 +0530

    gtgr

--- src/Const.java
+++ src/Const.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Const {
+import java.util.Scanner;
+
+//class cons{
+//    cons(){
+//        System.out.println("this is basic constructor");
+//    }
+//    int b=5;
+//    cons(int b){
+//        this.b=b;
+//        System.out.println("this is parmeterised constructor "+b);
+//    }
+//}
+class cons{
+    String s="deevyam";
+    int b=1234;
+    Scanner sc=new Scanner(System.in);
+    cons(){
+        System.out.println("Enter the bank details Name , account number");
+        String st=sc.next();
+        int b=sc.nextInt();
+    }
+    cons(String s,int b){
+        this.s=s;this.b=b;
+        System.out.println("account name"+s);
+        System.out.println("account number"+b);
+    }
+
+}
+public class Const {
+    public static void main(String[] args) {
+        cons c=new cons("deevyam",1234);
+
+    }
 }
--- src/Demo1.java
+++ src/Demo1.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Demo1 {
+import java.util.Scanner;
+
+class arr{
+    char[] ar;
+    Scanner sc=new Scanner(System.in);
+    void getdata(){
+        for(int i=0;i<5;i++){
+            ar=sc.next().toCharArray();
+        }
+    }
+}
+class Demo1 extends arr {
+   public void display(){
+        for(int i=0;i<ar.length;i++){
+            System.out.println(ar[i]);
+        }
+    }
+    public static void main(String[] args) {
+        Demo1 d=new Demo1();
+        d.getdata();
+        d.display();
+    }
 }
